3b78610d7d2676b9ffbaba72ac700cf9277e3acf
SGC41 <104068405+SGC41@users.noreply.github.com>
2024-05-06 22:02:32 +0200

    Update heartbeat.sh

--- heartbeat.sh
+++ heartbeat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
        
 while true; do
-sleep $((60*60))
+sleep $((60*3))
 rsync -avz --exclude={'/etc/hosts','/dev','/mnt','/proc','/sys','/media','/lost+found'} --delete / /media/persistent/backup
 # sleep 1 hour
 done
