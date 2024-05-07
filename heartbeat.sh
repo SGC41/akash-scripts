32f6c7ae7f3ef606c7855e1f60a24e4ac7cef941
SGC41 <104068405+SGC41@users.noreply.github.com>
2024-05-07 11:10:48 +0200

    Update heartbeat.sh
    script to manage recurring backup in akash deployment.

--- heartbeat.sh
+++ heartbeat.sh
@@ -2,7 +2,6 @@
        
 while true; do
 sleep $((60*3))
-rsync -avz --exclude={'/etc/hosts','/dev','/mnt','/proc','/sys','/media','/lost+found'} --delete / /media/persistent/backup
-# sleep 1 hour
+rsync -avz --exclude={'/etc/hostname','/etc/resolv.conf','/etc/hosts','/dev','/mnt','/proc','/sys','/media','/lost+found'} --delete / /media/persistent/backup
 done
         
